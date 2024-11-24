c12dbf6d9609cb9b64119d7f39b78f516f7f445d
= <=>
2024-11-24 19:33:07 +0000

    adjust grace period

--- backend/src/utils/constants.ts
+++ backend/src/utils/constants.ts
@@ -5,4 +5,4 @@ export const GRID_BIAS_WEIGHT: number = (GRID_ROW_SIZE * GRID_COLUMN_SIZE) * 0.2
 export const GRID_CHAR_LIST: string = 'abcdefghijklmnopqrstuvwxyz';
 export const GRID_BIAS_COOLDOWN: number = 4000; // 4 seconds
 export const GRID_STOP_INTERVAL: number = 60000; // 60 seconds
-export const GRID_GRACE_PERIOD: number = 2000; // 2000 milliseconds
+export const GRID_GRACE_PERIOD: number = 1700; // 2000 milliseconds
