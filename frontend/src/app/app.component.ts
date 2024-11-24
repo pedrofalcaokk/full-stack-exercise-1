ffeb25a73a8940bad77f0f7bd6bce8822001eb75
= <=>
2024-11-24 18:02:45 +0000

    make each component it's separate page and add navigation

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
-import { GridComponent } from './components/grid/grid.component';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, GridComponent],
+    standalone: true,
+    imports: [RouterOutlet, RouterLink, RouterLinkActive, MatToolbarModule, MatButtonModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
